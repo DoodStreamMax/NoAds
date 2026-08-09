1224d7d3150dad33c13178308b927586d09e2423
DoodStreamMax <142033079+DoodStreamMax@users.noreply.github.com>
2026-08-10 02:21:59 +0800

    Update PlayVideo.tsx

--- src/pages/PlayVideo.tsx
+++ src/pages/PlayVideo.tsx
@@ -37,7 +37,7 @@ export function PlayVideo() {
 
   const randomUrls = [
     'https://dulyhagglermounting.com/2082665',
-    'https://wwp.giriucat.com/redirect-zone/5137a848'
+    'https://sorrowfulpsychology.com/HE9TFh'
   ];
 
   useEffect(() => {
